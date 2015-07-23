32d0908ddf5b9dd15ad50a27bce84e34739e2a15
Aanand Prasad <aanand.prasad@gmail.com>
2015-07-23 14:35:17 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from __future__ import absolute_import
+from setuptools import setup, find_packages
+
+import codecs
+import os
+import re
+
+
+def read(*parts):
+    path = os.path.join(os.path.dirname(__file__), *parts)
+    with codecs.open(path, encoding='utf-8') as fobj:
+        return fobj.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+tests_require = [
+    'py.test >= 2.7.2, <3',
+]
+
+
+setup(
+    name='posixps',
+    version=find_version("posixps", "__init__.py"),
+    description='Implementation of a subset of POSIX parameter substitution',
+    packages=find_packages(exclude=['tests.*', 'tests']),
+    include_package_data=True,
+    tests_require=tests_require,
+)
